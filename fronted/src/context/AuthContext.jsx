f8902e4285dabcc5f4f00ca466e8c147df40176e
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-08-18 15:01:38 +0530

    feat: initialize fronted project with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created Vite configuration for React and Tailwind CSS
    - Implemented main application structure with App component
    - Added AuthContext for user authentication management
    - Included global styles with Tailwind CSS and custom CSS
    - Added SVG assets for React and Vite logos
    - Set up basic layout and styling for the application

---
+++ fronted/src/context/AuthContext.jsx
@@ -0,0 +1,76 @@
+import { useEffect,createContext, useState } from "react";
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [user, SetUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    const getCurrentUser = async () => {
+        try {
+            const res = await axios.get("/api/v1/users/current-user", {
+                headers : {
+                    Authorization : `Bearer ${localStorage.getItem('token')}`,
+                },
+            })
+            SetUser(res.data.data)
+        } catch (error) {
+            SetUser(null)
+        } finally{
+            setLoading(false)
+        }
+    };
+
+    const refreshUser = async () => {
+        await getCurrentUser();
+    }
+
+    const register = async (formData) => {
+        const res = await axios.post('/api/v1/users/register', formData, {
+            headers : { 'Content-Type' : 'multipart/form-data'},
+        });
+        await getCurrentUser();
+    };
+
+    const login = async (emailOrUsername, password) => {
+        try {
+            const res = await axios.post('/api/v1/users/login', {
+                email : emailOrUsername.includes('@') ? emailOrUsername : undefined,
+                username : !emailOrUsername.includes('@') ? emailOrUsername : undefined,
+                password,
+            });
+
+            SetUser(res.data.data);
+            navigate('/');
+        } catch (error) {
+            throw error.response?.data?.message || "Login failed"
+        }
+    };
+
+    const logout = async () => {
+        try {
+            await axios.post('/api/v1/users/logout');
+            SetUser(null);
+            navigate('/login')
+        } catch (error) {
+            console.error('Logout error', error)
+        }
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            getCurrentUser();
+        } else{
+            setLoading(false);
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{loading, SetUser, user, refreshUser, login, logout,register}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
