a3338cb9bde0d4c4bcb7aa0372f7ec63237c7f24
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-10-13 14:35:35 +0530

    se

--- backned/src/app.js
+++ backned/src/app.js
@@ -4,13 +4,20 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 
 const app = express ()
+// In your backend
+const corsOptions = {
+  origin: [
+    'https://dev-connect-109.vercel.app',
+    'https://dev-connect-coral.vercel.app/',
+    'https://dev-connect-coral.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+};
 
-app.use(
-    cors({
-        origin : ['https://dev-connect-coral.vercel.app', 'http://localhost:5173'],
-        credentials : true
-    })
-)
+app.use(cors(corsOptions));
+
 
 app.use(express.json({limit : "25mb"}))
 app.use(express.urlencoded({extended : true, limit : "15mb"}))
