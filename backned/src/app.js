b8b5160bdfcb2d95994bd53ec31507fac2b0a78b
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2026-02-12 12:51:21 +0530

    gg

--- backned/src/app.js
+++ backned/src/app.js
@@ -9,7 +9,7 @@ const app = express ()
 
 app.use(
     cors({
-        origin : [process.env.CORS_ORIGIN, 'http://localhost:5173'],
+        origin : ['https://dev-connect-coral.vercel.app', 'http://localhost:5173'],
         credentials : true
     })
 )
