3bca68bd6e124af901c064cf175365641baf6d5f
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2026-02-12 13:19:06 +0530

    n

--- backned/src/app.js
+++ backned/src/app.js
@@ -6,11 +6,13 @@ import morgan from 'morgan'
 const app = express ()
 
 
-//  {
-      //  origin : ['https://dev-connect-coral.vercel.app/api/v1', 'http://localhost:5173'],
-      //  credentials : true
-  //  }
-app.use(cors())
+
+app.use(
+    cors({
+        origin : ['https://dev-connect-coral.vercel.app', 'http://localhost:5173'],
+        credentials : true
+    })
+)
 
 app.use(express.json({limit : "25mb"}))
 app.use(express.urlencoded({extended : true, limit : "15mb"}))
