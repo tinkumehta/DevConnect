fdd671281a5469a53cc5ad8c8a1b103d6aa1e5b6
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2026-02-13 11:16:20 +0530

    env configer

--- backned/src/app.js
+++ backned/src/app.js
@@ -6,7 +6,7 @@ import morgan from 'morgan'
 const app = express ()
 
 const corsOptions = {
-  origin: ['https://dev-connect-coral.vercel.app', 'http://localhost:5173'],
+  origin: [process.env.CORS_ORIGIN, 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
