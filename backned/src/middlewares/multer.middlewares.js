591c91e5e63a8420a0e1de1c71e8d54b577421ce
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2026-02-18 21:22:20 +0530

    setup img

--- backned/src/middlewares/multer.middlewares.js
+++ backned/src/middlewares/multer.middlewares.js
@@ -1,22 +1,22 @@
 import multer from 'multer'
 
-// const storage = multer.diskStorage({
-//     destination : function (req, file, cb){
-//         cb(null, './public/temp')
-//     },
-//     filename : function (req, file, cb){
-//         cb(null, file.originalname)
-//     }
-// })
+const storage = multer.diskStorage({
+    destination : function (req, file, cb){
+        cb(null, './public/temp')
+    },
+    filename : function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
 
-// export const upload = multer({
-//     storage
-// })
+export const upload = multer({
+    storage
+})
 
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage();
 
-const upload = multer({storage});
+// const upload = multer({storage});
 
-export {
-    upload
-}
+// export {
+//     upload
+// }
